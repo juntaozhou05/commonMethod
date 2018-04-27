680ba13fd3bc4387ce56645a10ee6eafde2dbeed
juntaozhou05 <350529307@qq.com>
2018-04-27 14:42:29 +0800

    add methods

--- index2.js
+++ index2.js
@@ -28,3 +28,36 @@ for (var i = 1; i < a.length; i++) {
   o = o + a[i].slice(0, 1).toUpperCase() + a[i].slice(1);
 }
 console.log(o)
+
+//实现一个bind方法
+Function.prototype.bind = function (context) {
+  if (context.toString() !== "object Object" || context.toString() !== "object Window") {
+    return false;
+  }
+  let _this = this;
+  let args = [].slice.call(arguments, 1);
+  return function () {
+    _args = [].slice.call(arguments);
+    _this.apply(context, _args.concat(args));
+  }
+}
+
+//基础柯理化
+function curry(fn) {
+  if (typeof fn !== "function") {
+    return false;
+  }
+  let args = [].slice.call(arguments, 1);
+  return function () {
+    let _args = [].slice.call(arguments);
+    return fn.apply(this, _args.concat(args));
+  }
+}
+
+//改进柯理化
+function curry2(fn, args) {
+  if (typeof fn !== "function") {
+    return false;
+  }
+  let len = fn.length;
+}
