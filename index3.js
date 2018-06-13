1c88deeeb9e2322f6434c8ab984f0eb99b878b51
juntaozhou05 <350529307@qq.com>
2018-06-13 17:33:37 +0800

    add quicksort

--- index3.js
+++ index3.js
@@ -200,3 +200,58 @@ var addStrings = function (a, b) {
   }
   return result.reverse().join("");
 };
+
+//改进版快速排序
+function quick(array, left, right) {
+  let index;
+  if (array.length > 1) {
+    index = partition(array, left, right);
+    if (left < index - 1) {
+      quick(array, left, index - 1);
+    }
+    if (index < right) {
+      quick(array, index, right);
+    }
+  }
+  return array;
+}
+
+function quickSort(array) {
+  return quick(array, 0, array.length - 1);
+};
+// 划分操作函数
+function partition(array, left, right) {
+  // 用index取中间值而非splice
+  const pivot = array[Math.floor((right + left) / 2)];
+  let i = left;
+  let j = right;
+
+  while (i <= j) {
+    while (compare(array[i], pivot) === -1) {
+      i++;
+    }
+    while (compare(array[j], pivot) === 1) {
+      j--;
+    }
+    if (i <= j) {
+      swap(array, i, j);
+      i++;
+      j--;
+    }
+  }
+  return i;
+}
+// 原地交换函数，而非用临时数组
+function swap(array, a, b) {
+  [array[a], array[b]] = [array[b], array[a]];
+}
+
+// 比较函数
+function compare(a, b) {
+  if (a === b) {
+    return 0;
+  }
+  return a < b ? -1 : 1;
+}
+
+console.log(quickSort([1, 2, 4, 3, 6]));
