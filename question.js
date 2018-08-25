709effe8bbf9b7c5c730365dc0f5491b840ae2b5
zhoujuntao <350529307@qq.com>
2018-08-25 10:42:29 +0800

    add some question

--- question.js
+++ question.js
@@ -1,33 +1,36 @@
 //1
 function test(xxx) {
-  console.log(xxx);
-  var xxx = 123;
+  console.log(xxx); 
+  var xxx = 123; 
   function xxx() {}
-  console.log(xxx);
+  console.log(xxx); 
 }
-test(444);
+test(444); 
 
 //2
-var a = 1;
+var a = 1; 
 function test1() {
-  console.log(a);
+  console.log(a); 
 }
 function test2() {
-  var a = 2;
-  test1();
+  var a = 2; 
+  test1(); 
 }
-test2();
+test2(); 
 
 //3
 var fun = function() {
-  this.a = "fun";
-};
+  this.a = "fun"; 
+}; 
 var myfun = function(test) {
-  var a = "myfun";
-  console.log(this.a);
-  console.log(a);
-  console.log(test);
-};
-myfun.call(fun, "test");
+  var a = "myfun"; 
+  console.log(this.a); 
+  console.log(a); 
+  console.log(test); 
+}; 
+myfun.call(fun, "test"); 
 
 //输出1到10000的完美数
+
+//JS函数调用的四种方法
+//1.方法调用模式 2.函数调用模式 2.构造器调用模式 3.apply调用模式
